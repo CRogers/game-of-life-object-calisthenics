846cab365d4b788a699efd43b86e159af60f6ff0
Callum Rogers <callumrogers@hotmail.co.uk>
2015-12-24 17:38:43 +0000

    Refactor: Remove duplication from tests

--- src/test/java/GameShould.java
+++ src/test/java/GameShould.java
@@ -4,16 +4,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class GameShould {
+    private Game game = new Game();
+
     @Test
     public void start_off_with_a_score_of_0() {
-        Game game = new Game();
-        assertThat(game.score(), is(0));
+        scoreShouldBe(0);
     }
 
     @Test
     public void have_a_score_of_1_after_rolling_a_1() {
-        Game game = new Game();
         game.roll(1);
-        assertThat(game.score(), is(1));
+        scoreShouldBe(1);
+    }
+
+    private void scoreShouldBe(int score) {
+        assertThat(game.score(), is(score));
     }
 }
