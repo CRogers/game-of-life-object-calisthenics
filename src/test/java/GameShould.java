7018c54264117f28e1047ef948085a15f5da1e39
Callum Rogers <callumrogers@hotmail.co.uk>
2016-11-02 18:52:54 +0000

    Add a whole load of tests

--- src/test/java/GameShould.java
+++ src/test/java/GameShould.java
@@ -1,3 +1,4 @@
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -5,12 +6,15 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(Parameterized.class)
 public class GameShould {
     private static final int STRIKE = 10;
+    private static final List<Integer> SPARE = ImmutableList.of(5, 5);
 
     private final Game game = new Game();
 
@@ -25,6 +29,12 @@ public class GameShould {
             { rolls(2, 5), 7 },
             { rolls(STRIKE), 10 },
             { rolls(STRIKE, 1), 12 },
+            { rolls(STRIKE, 1, 3), 18 },
+            { rolls(SPARE), 10 },
+            { rolls(SPARE, 2), 14 },
+            { rolls(SPARE, 2, 3), 17 },
+            { rolls(Stream.generate(() -> STRIKE).limit(12).collect(toList())), 300 },
+
         });
     }
 
@@ -39,8 +49,14 @@ public class GameShould {
         assertThat(game.score()).isEqualTo(finalScore);
     }
 
-    private static List<Integer> rolls(Integer... rolls) {
-        return Arrays.asList(rolls);
+    private static List<Integer> rolls(Object... rolls) {
+        return Arrays.asList(rolls).stream()
+            .flatMap(obj -> {
+                if (obj instanceof List) {
+                    return ((List<Integer>) obj).stream();
+                }
+                return Stream.of((Integer) obj);
+            }).collect(toList());
     }
 
 }
