2e13f3963ae4e81332985973d8e45c79cbd8cec9
Callum Rogers <callumrogers@hotmail.co.uk>
2016-11-02 18:03:58 +0000

    Go back to the beginning

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,10 +2,7 @@ public class Game {
     private int score = 0;
 
     public void roll(int pins) {
-        if (score == 10) {
-            score += pins;
-        }
-        score += pins;
+
     }
 
     public int score() {
--- src/test/java/GameShould.java
+++ src/test/java/GameShould.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameShould {
     private Game game = new Game();
@@ -11,44 +10,7 @@ public class GameShould {
         scoreShouldBe(0);
     }
 
-    @Test
-    public void have_a_score_of_1_after_rolling_a_1() {
-        game.roll(1);
-        scoreShouldBe(1);
-    }
-
-    @Test
-    public void have_a_score_of_2_after_rolling_a_2() {
-        game.roll(2);
-        scoreShouldBe(2);
-    }
-
-    @Test
-    public void have_a_score_of_3_after_rolling_a_1_then_a_2() {
-        game.roll(1);
-        game.roll(2);
-        scoreShouldBe(3);
-    }
-
-    @Test
-    public void have_a_score_of_14_after_rolling_a_spare_then_a_2() {
-        game.roll(4);
-        game.roll(6);
-        game.roll(2);
-        scoreShouldBe(14);
-    }
-
-    @Test
-    public void have_a_score_of_15_after_scoring_1_for_the_first_frame_then_a_spare_then_a_2() {
-        game.roll(1);
-        game.roll(0);
-        game.roll(4);
-        game.roll(6);
-        game.roll(2);
-        scoreShouldBe(15);
-    }
-
     private void scoreShouldBe(int score) {
-        assertThat(game.score(), is(score));
+        assertThat(game.score()).isEqualTo(score);
     }
 }
